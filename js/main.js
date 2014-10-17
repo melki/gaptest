9e92126501e6b5e369d3feab70d53d9678dcc225
Melki <melchior.toldi@gmail.com>
2014-10-17 16:36:07 +0200

    notif2

--- js/main.js
+++ js/main.js
@@ -22,8 +22,10 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new LocalStorageStore();
-         self.showAlert('Store Initialized', 'Info');
+        
+        this.store = new LocalStorageStore(function() {
+        self.showAlert('Store Initialized', 'Info');
+    }); 
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
